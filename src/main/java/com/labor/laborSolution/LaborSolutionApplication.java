400bad33142ad7a4f237f9d575a893e3ece44013
seong <seong7577@naver.com>
2025-03-31 20:52:36 +0900

    스프링부트 + 리액트 + 타입스크립트 설정부터 문제

---
+++ src/main/java/com/labor/laborSolution/LaborSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.labor.laborSolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaborSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaborSolutionApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/labor/laborSolution/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.labor.laborSolution.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+ * @Configuration
+ * 
+ * @EnableWebMvc public class WebMvcConfig implements WebMvcConfigurer {
+ * 
+ * @Override public void addCorsMappings(CorsRegistry registry) { // TODO
+ * Auto-generated method stub registry.addMapping("/**")
+ * .allowedOrigins("http://localhost:3000") .allowedMethods("OPTIONS", "GET",
+ * "POST", "PUT", "DELETE");
+ * 
+ * }
+ * 
+ * 
+ * }
+ */
